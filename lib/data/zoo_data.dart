75c2b5cc32ddff7cac2c4ba08b1ea7504abfa77c
Marshell <neechan354@gmail.com>
2023-12-21 10:12:45 +0700

    Update zoo_data.dart
    Tambah Gambar. galeri

--- lib/data/zoo_data.dart
+++ lib/data/zoo_data.dart
@@ -12,6 +12,9 @@ var zooList = [
     imageAsset: 'images/Safari.jpg',
     imageUrls: [
       'https://img.harianjogja.com/posts/2023/12/10/1157755/wisata_puncak_-_taman_safari_indonesia_1671529719.jpg',
+      'https://news.tamansafari.com/Newsportalv22ok/upload/83_Prigen_Kecil.jpg',
+      'https://www.blibli.com/friends-backend/wp-content/uploads/2022/12/panduan-wisata-taman-safari-indonesia-bali.jpeg,',
+
 
     ],
     isFavorite: false,
